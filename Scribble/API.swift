9e229f566ca3090e3d89dde16635b692f9c76c32
Atul Madhugiri <atulm@berkeley.edu>
2023-11-19 16:10:34 -0500

    [macos] Move `NetworkManager` into separate file

--- Scribble/API.swift
+++ Scribble/API.swift
@@ -1,26 +1,5 @@
 import Foundation
 
-struct NetworkManager {
-
-  static func sendRequest<T: Codable>(to url: URL, with body: T) async throws -> Data {
-    var request = URLRequest(url: url)
-    request.httpMethod = "POST"
-    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.addValue("Bearer \(Secrets.OPENAI_API_KEY)", forHTTPHeaderField: "Authorization")
-
-    request.httpBody = try JSONEncoder().encode(body)
-
-    let (data, response) = try await URLSession.shared.data(for: request)
-    guard let httpResponse = response as? HTTPURLResponse,
-      (200...299).contains(httpResponse.statusCode)
-    else {
-      throw URLError(.badServerResponse)
-    }
-    return data
-  }
-
-}
-
 enum ImageModel: String, Codable {
   case dalle2 = "dall-e-2"
   case dalle3 = "dall-e-3"
---
+++ Scribble/NetworkManager.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+struct NetworkManager {
+
+  static func sendRequest<T: Codable>(to url: URL, with body: T) async throws -> Data {
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.addValue("Bearer \(Secrets.OPENAI_API_KEY)", forHTTPHeaderField: "Authorization")
+
+    request.httpBody = try JSONEncoder().encode(body)
+
+    let (data, response) = try await URLSession.shared.data(for: request)
+    guard let httpResponse = response as? HTTPURLResponse,
+      (200...299).contains(httpResponse.statusCode)
+    else {
+      throw URLError(.badServerResponse)
+    }
+    return data
+  }
+
+}
