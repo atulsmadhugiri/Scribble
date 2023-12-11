330d4faae3329ebbc1f01306f6bfffe7c77a7f2a
Atul Madhugiri <atulm@berkeley.edu>
2023-12-11 00:44:54 -0500

    [ios] Introduce `ImageBrowsingView`
    Basically separate out the browsing into separate tab bar tab so things aren't as cluttered.

--- ScribbleMobile/ImageCreationView.swift
+++ ScribbleMobile/ImageCreationView.swift
@@ -14,12 +14,6 @@ struct ImageCreationView: View {
   @State private var selectedModel: ImageModel = .dalle3
   @State private var selectedQuality: ImageQuality = .hd
 
-  @State private var searchTerm = ""
-  var filteredEntries: [GeneratedImage] {
-    guard !searchTerm.isEmpty else { return entries }
-    return entries.filter { $0.revised_prompt.localizedCaseInsensitiveContains(searchTerm) }
-  }
-
   var body: some View {
 
     VStack {
@@ -104,40 +98,6 @@ struct ImageCreationView: View {
         Text("Standard").tag(ImageQuality.standard)
         Text("HD").tag(ImageQuality.hd)
       }.pickerStyle(.segmented).labelsHidden()
-
-      Divider()
-
-      NavigationView {
-        List {
-          ForEach(filteredEntries, id: \.id) { entry in
-            HStack {
-              AsyncImage(url: URL(string: entry.url)) { image in
-                image.interpolation(.none).resizable().scaledToFit().cornerRadius(8).frame(
-                  width: 100, height: 100
-                ).transition(.opacity.animation(.default)).onDrag {
-
-                  let existingURL = URL(string: entry.url)
-                  do {
-                    return try getItemProvider(for: existingURL!)
-                  } catch {
-                    print("Unable to get NSItemProvider for existingURL")
-                  }
-                  return NSItemProvider()
-                }
-
-              } placeholder: {
-                ZStack {
-                  ProgressView().progressViewStyle(CircularProgressViewStyle())
-                  Color.gray.opacity(0.1).cornerRadius(8).frame(width: 100, height: 100)
-                }
-              }
-              Text(entry.revised_prompt).font(.footnote).lineLimit(6)
-            }
-          }
-        }.searchable(text: $searchTerm, placement: .automatic, prompt: "")
-          .listStyle(.plain)
-          .navigationBarTitle("Image Generations", displayMode: .inline)
-      }
 
     }.padding()
   }
